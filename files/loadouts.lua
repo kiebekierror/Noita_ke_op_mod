ce8a5f58c5ccc50a6141a82be02ee5ccd878427a
kiebekierror <liobiznes@outlook.com>
2023-12-08 09:21:23 +0800

    modified foo

--- files/loadouts.lua
+++ files/loadouts.lua
@@ -1,15 +1,19 @@
 loadout_list =
 {
-	{
-		name = "a Summoner TYPE",
+	--[[{
+		name = "5 in one",
 		folder = "summoner",
+		-- folder = "slime",
+		-- folder = "thunder",
+		-- folder = "eldritch",
+		-- folder = "butcher",
 		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
 		cape_color = 0xff60a1a2,
 		cape_color_edge = 0xff3c696a,
 		items = 
 		{
-			"mods/starting_loadouts/files/summoner/wands/wand_1.xml",
-			"mods/starting_loadouts/files/summoner/wands/wand_2.xml",
+			"mods/mymod/files/summoner/wands/wand_1.xml",
+			"mods/mymod/files/summoner/wands/wand_2.xml",
 			"data/entities/items/pickup/potion_water.xml",
 			{ 
 			options = { "data/entities/items/pickup/egg_fire.xml", "data/entities/items/pickup/egg_red.xml", "data/entities/items/pickup/egg_monster.xml", "data/entities/items/pickup/egg_slime.xml" },
@@ -18,122 +22,147 @@ loadout_list =
 		},
 		perks =
 		{
-			"PLAGUE_RATS",
+			"BLEED_OIL",
+			"BLEED_SLIME",
+			"ELECTRICITY",
+			"GLOBAL_GORE",
 			"HOMUNCULUS",
+			"PLAGUE_RATS",
+			"PROJECTILE_REPULSION",
+			"REVENGE_TENTACLE",
+			"STRONG_KICK",
 		},
-	},
+	},]]
 	{
-		name = "a Fire TYPE",
-		folder = "fire",
+		name = "a Element TYPE",
+		folder = "thunder",
 		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
-		cape_color = 0xff5a60dd,
-		cape_color_edge = 0xff3e43af,
-		items = 
+		cape_color = 0xff9d7b4d,
+		cape_color_edge = 0xff846235,
+		--[[items =
 		{
-			"mods/starting_loadouts/files/fire/wands/wand_1.xml",
-			"mods/starting_loadouts/files/fire/wands/wand_2.xml",
-			{
-			"mods/starting_loadouts/files/fire/potion_fire.xml",
-			amount = 2,
-			},
+			"mods/mymod/files/thunder/wands/wand_1.xml",
+			"mods/mymod/files/thunder/wands/wand_2.xml",
+			"data/entities/items/pickup/thunderstone.xml",
+		},]]
+		items =
+		{
+			"mods/starting_loadouts/files/summoner/wands/wand_2.xml",
+			"mods/starting_loadouts/files/summoner/wands/wand_1.xml",
+			"data/entities/items/pickup/potion_water.xml",
 		},
 		perks =
 		{
 			"BLEED_OIL",
+			"BLEED_SLIME",
+			"GLOBAL_GORE",
+			"GOLD_IS_FOREVER",
+			"HOMUNCULUS",
+			"HOMUNCULUS",
+			-- "LUKKI_MINION",
+			"MAP",
+			"MELT",											-- ?
+			"MOON_RADAR",
+			"MOVEMENT_FASTER",
+			"ORBIT",										-- ?
+			"PEACE_WITH_STEVE",
+			"PERMANENT_FLYING",								--?
+			"PLAGUE_RATS",
+			"PROJECTILE_HOMING",
+			"PROJECTILE_DUPLICATE",
+			"PROJECTILE_EATER_SECTOR",
+			"PROJECTILE_REPULSION_SECTOR",
+			"PROJECTILE_SLOW_FIELD",
+			"PROJECTILE_REPULSION",
+			"PROJECTILE_REPULSION",
+			"PROTECTION_ELECTRICITY",
+			"PROTECTION_EXPLOSION",
+			"PROTECTION_FIRE",
+			"PROTECTION_FREEZE",
+			"PROTECTION_MELEE",
+			"PROTECTION_RADIOACTIVITY",
+			"RADAR_ENEMY",
+			"RADAR_ITEM",
+			"RADAR_MOON",
+			"RADAR_WAND",
+			"STRONG_KICK",
+			"VAMPIRISM",
+			-- added
+
+			-- "ATTACK_FOOT",
+			"ATTRACT_ITEMS",
+			"BREATH_UNDERWATER",
+			"ADVENTURER",
+			"ADVENTURER",
+			"EDIT_WANDS_EVERYWHERE",
+			"FAST_PROJECTILES",
+			"FASTER_LEVITATION",
+			"FASTER_SWIMMING",
+			"GOLDEN_I",
+			"HUNGRY_GHOST",
+			"LIQUID_MOVEMENT",								-- ?
+			"SHIELD",
+
+			-- "TELEPORTITIS",
+			"TELEPORTITIS_DODGE",
+
+			"ALWAYS_CAST",
+			"TELEKINESIS",
+			"ALWAYS_CAST",
+			"UNLIMITED_SPELLS",
+			"ALWAYS_CAST",
+
+			"EXPLODING_GOLD",
+			-- "EXTRA_ACTION_STORAGE",
+			-- "EXTRA_ACTION_STORAGE",
+			"EXTRA_PERK",
+			"EXTRA_PERK",
+			"EXTRA_PERK",
+			"EXTRA_WAND_SLOT",
+			"EXTRA_WAND_SLOT",
+
+			"HEARTS_MORE_EXTRA_HP",
+			"HEARTS_MORE_EXTRA_HP",
+			"HEARTS_MORE_EXTRA_HP",
+			"HEARTS_MORE_EXTRA_HP",
+			-- "EXTRA_HP",
+			-- "EXTRA_HP",
+			"EXTRA_SLOTS",
+			"EXTRA_SLOTS",
+
+			"REMOVE_FOG_OF_WAR",
 		},
 	},
-	{
-		name = "a Slime TYPE",
+	--[[{
+		name = "K.E. TYPE",
 		folder = "slime",
 		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
 		cape_color = 0xff9a6f9b,
 		cape_color_edge = 0xff76547f,
 		items = 
 		{
-			"mods/starting_loadouts/files/slime/wands/wand_1.xml",
-			"mods/starting_loadouts/files/slime/wands/wand_2.xml",
-			"mods/starting_loadouts/files/slime/wands/wand_3.xml",
+			"mods/mymod/files/slime/wands/wand_1.xml",
+			"mods/mymod/files/slime/wands/wand_2.xml",
+			"mods/mymod/files/slime/wands/wand_3.xml",
 			{
-			"mods/starting_loadouts/files/slime/potion_slime.xml",
+			"mods/mymod/files/								i/potion_slime.xml",
 			amount = 2,
 			},
 		},
 		perks =
 		{
+			"BLEED_OIL",
 			"BLEED_SLIME",
-		},
-	},
-	{
-		name = "a Thunder TYPE",
-		folder = "thunder",
-		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
-		cape_color = 0xff9d7b4d,
-		cape_color_edge = 0xff846235,
-		items = 
-		{
-			"mods/starting_loadouts/files/thunder/wands/wand_1.xml",
-			"mods/starting_loadouts/files/thunder/wands/wand_2.xml",
-			"data/entities/items/pickup/thunderstone.xml",
-		},
-		perks =
-		{
-			"ELECTRICITY",
-		},
-	},
-	{
-		name = "an Eldritch TYPE",
-		folder = "eldritch",
-		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
-		cape_color = 0xff7d4e53,
-		cape_color_edge = 0xff6b4144,
-		items = 
-		{
-			"mods/starting_loadouts/files/eldritch/wands/wand_1.xml",
-			"mods/starting_loadouts/files/eldritch/wands/wand_2.xml",
-			"mods/starting_loadouts/files/eldritch/wands/wand_3.xml",
-			"mods/starting_loadouts/files/eldritch/potion_teleport.xml",
-			"data/entities/items/pickup/potion_water.xml",
-		},
-		perks =
-		{
-			"REVENGE_TENTACLE",
-			"PROJECTILE_REPULSION",
-		},
-	},
-	{
-		name = "a Butcher TYPE",
-		folder = "butcher",
-		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
-		cape_color = 0xff4f626b,
-		cape_color_edge = 0xff465258,
-		items = 
-		{
-			"mods/starting_loadouts/files/butcher/wands/wand_1.xml",
-			"mods/starting_loadouts/files/butcher/wands/wand_2.xml",
-			"mods/starting_loadouts/files/butcher/potion_berserk.xml",
-			"mods/starting_loadouts/files/butcher/potion_blood.xml",
-		},
-		perks =
-		{
 			"GLOBAL_GORE",
+			"PLAGUE_RATS",
+			"PROJECTILE_REPULSION",
+			"PROTECTION_ELECTRICITY",
+			"PROTECTION_EXPLOSION",
+			"PROTECTION_FIRE",
+			"PROTECTION_FREEZE",
+			"PROTECTION_MELEE",
+			"PROTECTION_RADIOACTIVITY",
 			"STRONG_KICK",
 		},
-	},
-	--[[
-	{
-		name = "Sludge warlock",
-	},
-	{
-		name = "Master transmuter",
-	},
-	{
-		name = "Ricochet druid",
-	},
-	{
-		name = "Teleport wizard",
-	},
-	{
-		name = "Engineer",
-	},
-	]]--
+	}]]
 }
--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-dofile( "mods/starting_loadouts/files/loadouts.lua" )
+dofile( "mods/mymod/files/loadouts.lua" )
 dofile( "data/scripts/perks/perk.lua" )
 
 function OnPlayerSpawned( player_entity ) -- this runs when player entity has been created
@@ -52,12 +52,12 @@ function OnPlayerSpawned( player_entity ) -- this runs when player entity has be
 	
 	-- set player sprite (since we change only one value, ComponentSetValue is fine)
 	local player_sprite_component = EntityGetFirstComponent( player_entity, "SpriteComponent" )
-	local player_sprite_file = "mods/starting_loadouts/files/" .. loadout_choice.folder .. "/player.xml"
+	local player_sprite_file = "mods/mymod/files/" .. loadout_choice.folder .. "/player.xml"
 	ComponentSetValue( player_sprite_component, "image_file", player_sprite_file )
 	
 	-- set player arm sprite
 	local player_arm_sprite_component = EntityGetFirstComponent( player_arm, "SpriteComponent" )
-	local player_arm_sprite_file = "mods/starting_loadouts/files/" .. loadout_choice.folder .. "/player_arm.xml"
+	local player_arm_sprite_file = "mods/mymod/files/" .. loadout_choice.folder .. "/player_arm.xml"
 	ComponentSetValue( player_arm_sprite_component, "image_file", player_arm_sprite_file )
 	
 	-- set player cape colour (since we're changing multiple variables, we'll use the edit_component() utility)
@@ -68,7 +68,7 @@ function OnPlayerSpawned( player_entity ) -- this runs when player entity has be
 	
 	-- set player ragdoll
 	local player_ragdoll_component = EntityGetFirstComponent( player_entity, "DamageModelComponent" )
-	local player_ragdoll_file = "mods/starting_loadouts/files/" .. loadout_choice.folder .. "/ragdoll/filenames.txt"
+	local player_ragdoll_file = "mods/mymod/files/" .. loadout_choice.folder .. "/ragdoll/filenames.txt"
 	ComponentSetValue( player_ragdoll_component, "ragdoll_filenames_file", player_ragdoll_file )
 
 	-- set inventory contents
@@ -122,15 +122,50 @@ function OnPlayerSpawned( player_entity ) -- this runs when player entity has be
 		end	
 	end
 	
+	-- perk_pickup( perk_entity, player_entity, EntityGetName( perk_entity ), false, false )
+	perk_pickup( "GLOBAL_GORE", player_entity, EntityGetName("GLOBAL_GORE"), false, false )
+	perk_pickup( "VAMPIRISM", player_entity, EntityGetName("VAMPIRISM"), false, false )
+	perk_pickup( "CHAINSAW", player_entity, EntityGetName("CHAINSAW"), false, false )
+	perk_pickup( "ATTACK_FOOT", player_entity, EntityGetName("ATTACK_FOOT"), false, false )
+	perk_pickup( "REPELLING_CAPE", player_entity, EntityGetName("REPELLING_CAPE"), false, false )
+	perk_pickup( "STAINLESS_ARMOUR", player_entity, EntityGetName("STAINLESS_ARMOUR"), false, false )
+	perk_pickup( "FREEZE_FIELD", player_entity, EntityGetName("FREEZE_FIELD"), false, false )
+	perk_pickup( "WAND_EXPERIMENTER", player_entity, EntityGetName("WAND_EXPERIMENTER"), false, false )
+	perk_pickup( "GLASS_CANNON", player_entity, EntityGetName("GLASS_CANNON"), false, false )
+	perk_pickup( "INVISIBILITY", player_entity, EntityGetName("INVISIBILITY"), false, false )
+	perk_pickup( "ELECTRICITY", player_entity, EntityGetName("ELECTRICITY"), false, false )
+	perk_pickup( "BREATH_UNDERWATER", player_entity, EntityGetName("BREATH_UNDERWATER"), false, false )
+	perk_pickup( "UNLIMITED_SPELLS", player_entity, EntityGetName("UNLIMITED_SPELLS"), false, false )
+	perk_pickup( "SEA_WATER", player_entity, EntityGetName("SEA_WATER"), false, false )
+	perk_pickup( "SPEED_DIVER", player_entity, EntityGetName("SPEED_DIVER"), false, false )
+	perk_pickup( "WORM_ATTRACTOR", player_entity, EntityGetName("WORM_ATTRACTOR"), false, false )
+	perk_pickup( "PROTECTION_MELEE", player_entity, EntityGetName("PROTECTION_MELEE"), false, false )
+	perk_pickup( "LEVITATION_TRAIL", player_entity, EntityGetName("LEVITATION_TRAIL"), false, false )
+	perk_pickup( "PROJECTILE_HOMING", player_entity, EntityGetName("PROJECTILE_HOMING"), false, false )
+	perk_pickup( "NO_WAND_EDITING", player_entity, EntityGetName("NO_WAND_EDITING"), false, false )
+	perk_pickup( "MOVEMENT_FASTER", player_entity, EntityGetName("MOVEMENT_FASTER"), false, false )
+	perk_pickup( "LOW_HP_DAMAGE_BOOST", player_entity, EntityGetName("LOW_HP_DAMAGE_BOOST"), false, false )
+	perk_pickup( "BLEED_OIL", player_entity, EntityGetName("BLEED_OIL"), false, false )
+	perk_pickup( "TRICK_BLOOD_MONEY", player_entity, EntityGetName("TRICK_BLOOD_MONEY"), false, false )
+	perk_pickup( "EXTRA_MONEY_TRICK_KILL", player_entity, EntityGetName("EXTRA_MONEY_TRICK_KILL"), false, false )
+	perk_pickup( "EXTRA_MONEY", player_entity, EntityGetName("EXTRA_MONEY"), false, false )
+	perk_pickup( "STRONG_KICK", player_entity, EntityGetName("STRONG_KICK"), false, false )
+	perk_pickup( "PROTECTION_FIRE", player_entity, EntityGetName("PROTECTION_FIRE"), false, false )
+	perk_pickup( "PROTECTION_EXPLOSION", player_entity, EntityGetName("PROTECTION_EXPLOSION"), false, false )
+	perk_pickup( "NUKE", player_entity, EntityGetName("NUKE"), false, false )
+	perk_pickup( "DISC_BULLET", player_entity, EntityGetName("DISC_BULLET"), false, false )
+	perk_pickup( "DISC_BULLET_BIG", player_entity, EntityGetName("DISC_BULLET_BIG"), false, false )
+
+
 	GamePrintImportant( "You're " .. loadout_name .. "!", "" )
 end
 
 -- Stainable sprites should have a corresponding SPRITE_NAME_uv_src.png next to the sprite file, and the folder containing the sprite should be passed to ModDevGenerateSpriteUVsForDirectory().
 -- For example for 'player.png' the corresponding UV source file is called 'player_uv_src.png'
 -- ModDevGenerateSpriteUVsForDirectory() must be called in init.lua file scope. It doesn't do anything outside noita_dev.exe.
-ModDevGenerateSpriteUVsForDirectory( "mods/starting_loadouts/files/fire" ) 
-ModDevGenerateSpriteUVsForDirectory( "mods/starting_loadouts/files/slime" ) 
-ModDevGenerateSpriteUVsForDirectory( "mods/starting_loadouts/files/summoner" ) 
-ModDevGenerateSpriteUVsForDirectory( "mods/starting_loadouts/files/thunder" ) 
-ModDevGenerateSpriteUVsForDirectory( "mods/starting_loadouts/files/eldritch" ) 
-ModDevGenerateSpriteUVsForDirectory( "mods/starting_loadouts/files/butcher" ) 
+ModDevGenerateSpriteUVsForDirectory( "mods/mymod/files/fire" )
+ModDevGenerateSpriteUVsForDirectory( "mods/mymod/files/slime" )
+ModDevGenerateSpriteUVsForDirectory( "mods/mymod/files/summoner" )
+ModDevGenerateSpriteUVsForDirectory( "mods/mymod/files/thunder" )
+ModDevGenerateSpriteUVsForDirectory( "mods/mymod/files/eldritch" )
+ModDevGenerateSpriteUVsForDirectory( "mods/mymod/files/butcher" )
